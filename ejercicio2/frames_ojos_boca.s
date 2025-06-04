9275ecdd098e0046109f1889d694fc05aef05268
LaraAyoub05 <135558784+LaraAyoub05@users.noreply.github.com>
2025-06-04 19:17:35 -0300

    agregar frames

---
+++ ejercicio2/frames_ojos_boca.s
@@ -0,0 +1,33 @@
+.include "colores.s"
+
+ojos_boca_frame1_data:
+    .word 244, 195, 23, 7
+    .quad BLACK 
+    .word 305, 195, 23, 7  
+    .quad BLACK   
+    .word 282, 217, 8, 8    
+    .quad BLACK 
+    .word 0, 0, 0, 0    
+    .quad 0             
+ojos_boca_frame2_data:
+    .word 244, 194, 23, 7
+    .quad BLACK 
+    .word 305, 194, 23, 7  
+    .quad BLACK 
+    .word 280, 215, 12, 12    
+    .quad BLACK
+    .word 284, 219, 4, 4
+    .quad VERDE_RAROVICH 
+    .word 0, 0, 0, 0  
+    .quad 0  
+ojos_boca_frame3_data:
+    .word 244, 194, 23, 7
+    .quad BLACK 
+    .word 305, 194, 23, 7  
+    .quad BLACK 
+    .word 275, 215, 23, 12    
+    .quad BLACK
+    .word 279, 219, 15, 4    
+    .quad VERDE_RAROVICH 
+    .word 0, 0, 0, 0  
+    .quad 0  
