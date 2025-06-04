4803ce93338dbacb1c21efdf84f9bafd8e1f7d7f
lunitarusso <russomauri.lunaa@gmail.com>
2025-06-04 19:32:38 -0300

    zzz de bmo

---
+++ ejercicio2/frames_zzz.s
@@ -0,0 +1,32 @@
+.include "colores.s"
+
+zzz_frame1_data:
+    .word 176, 96, 15, 7
+    .quad Z 
+    .word 184, 111, 15, 8  
+    .quad Z   
+    .word 184, 103, 7, 8    
+    .quad Z 
+    .word 0, 0, 0, 0    
+    .quad 0             
+zzz_frame2_data:
+    .word 138, 65, 23, 31
+    .quad Z 
+    .word 138, 73, 8, 8  
+    .quad VERDE_FONDO 
+    .word 153, 80, 8, 9    
+    .quad VERDE_FONDO
+    .word 0, 0, 0, 0  
+    .quad 0  
+zzz_frame3_data:
+    .word 108, 28, 30, 7
+    .quad Z 
+    .word 122, 35, 9, 8  
+    .quad Z 
+    .word 108, 51, 30, 7    
+    .quad Z
+    .word 115, 43, 8, 8    
+    .quad Z 
+    .word 0, 0, 0, 0  
+    .quad 0  
+ 
